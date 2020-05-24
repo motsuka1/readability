af955606bb8e9bfeffa0b430a7f82b1ba682d78c
Masaki Otsuka <motsuka@soka.edu>
2020-05-24 21:55:28 +0900

    count letters and words
    I added two functions that count the number of letters and words
    
    I use the Coleman-Liau index (index = 0.0588 * L - 0.296 * S - 15.8) to calculate the grade level of sentences. L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text. Thus, I needed to add the functions to count the number of letters and words. 
    
    I will create a function that counts the number of sentences.

---
+++ readability.c
@@ -0,0 +1,52 @@
+// evaluate grade level needed to comprehend some texts
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string argv);
+int count_words(string argv);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    printf("%lu length of string\n", strlen(text));
+    printf("%i letter(s)\n", count_letters(text));
+    printf("%i word(s)\n", count_words(text));
+}
+
+//Letters can be any uppercase or lowercase alphabetic characters, but shouldn’t include any punctuation, digits, or other symbols.
+int count_letters(string argv)
+{
+    int num_letters = 0;
+    for(int i = 0, n = strlen(argv); i < n; i++)
+    {
+        if((64 < argv[i] && argv[i] < 91) || (96 < argv[i] && argv[i] < 123))
+        {
+            num_letters += 1;
+        }
+        else
+        {
+            num_letters += 0;
+        }
+    }
+    return num_letters;
+}
+
+int count_words(string argv)
+{
+    int num_spaces = 0;
+    for(int i = 0, n = strlen(argv); i < n; i++)
+    {
+        if(argv[i] == 32)
+        {
+            num_spaces += 1;
+        }
+        else
+        {
+            num_spaces += 0;
+        }
+    }
+    int num_words = num_spaces + 1;
+    return num_words;
+}
