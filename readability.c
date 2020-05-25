03ae5b5f1fcba17169eb2b4344eb6c046e9cd1c8
Masaki Otsuka <motsuka@soka.edu>
2020-05-25 21:18:31 +0900

    fix count sentences and add grade level
    I fixed the count-sentences function and added the evaluate-grade-level function
    
    I forgot to add "I" and "?" as the signs of the end of sentences in the previous commit, so I added them. 
    
    I also added the function to evaluate grade level, using the Coleman-Liau index. 
    
    These fix/addition make this cs50 assignment complete.

--- readability.c
+++ readability.c
@@ -3,18 +3,32 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int count_letters(string argv);
 int count_words(string argv);
 int count_sentences(string argv);
+int evaluate_grade_level(string argv);
 
 int main(void)
 {
     string text = get_string("Text: ");
-    printf("%lu length of string\n", strlen(text));
-    printf("%i letter(s)\n", count_letters(text));
-    printf("%i word(s)\n", count_words(text));
-    printf("%i sentence(s)\n", count_sentences(text));
+    // printf("%lu length of string\n", strlen(text));
+    // printf("%i letter(s)\n", count_letters(text));
+    // printf("%i word(s)\n", count_words(text));
+    // printf("%i sentence(s)\n", count_sentences(text));
+    if(evaluate_grade_level(text) < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if(evaluate_grade_level(text) > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", evaluate_grade_level(text));
+    }
 }
 
 //Letters can be any uppercase or lowercase alphabetic characters, but shouldn’t include any punctuation, digits, or other symbols.
@@ -58,7 +72,7 @@ int count_sentences(string argv)
     int num_sentences = 0;
     for(int i = 0, n = strlen(argv); i < n; i++)
     {
-        if(argv[i] == 46)
+        if(argv[i] == 46 || argv[i] == 33 || argv[i] == 63)
         {
             num_sentences += 1;
         }
@@ -68,4 +82,13 @@ int count_sentences(string argv)
         }
     }
     return num_sentences;
+}
+
+int evaluate_grade_level(string argv)
+{
+    float L = count_letters(argv) * 100 / count_words(argv);
+    float S = count_sentences(argv) *  100 / count_words(argv);
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+    int grade_level = round(index);
+    return grade_level;
 }
